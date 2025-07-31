9a99383e3f133d3ce22218afb2dbbe9424bf3a07
Ford Berry <fordaberry@gmail.com>
2025-07-31 17:34:23 -0600

    Store book objects into an array and create constructor for books

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+const myLibrary = []; // Array to store book objects
+
+function Book(id, title, author, pages, read) {
+  // the constructor...
+  this.id = crypto.randomUUID;
+  this.title;
+  this.author;
+  this.pages;
+  this.read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  // take params, create a book then store it in the array
+}
