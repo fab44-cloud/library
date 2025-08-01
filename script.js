43f18f261dceec16d88db03ee4f4882a164e9d01
Ford Berry <fordaberry@gmail.com>
2025-07-31 20:34:33 -0600

    Create book and store it in an array

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const myLibrary = []; // Array to store book objects
 
-function Book(id, title, author, pages, read) {
+function Book(title, author, pages, read) {
   // the constructor...
   this.id = crypto.randomUUID;
-  this.title;
-  this.author;
-  this.pages;
-  this.read;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
+  let newBook = new Book(title, author, pages, read)
+  myLibrary.push(newBook)
+  console.log("New book added:", newBook);
 }
+
+addBookToLibrary("Harry Potter and the Sorcerers Stone", "J.K. Rowling",
+    309, true);
+
+console.log("Current library:", myLibrary);
